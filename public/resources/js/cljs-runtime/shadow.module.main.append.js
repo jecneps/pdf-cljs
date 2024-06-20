10342ac6060665172f9aaa11bd006157fa6144b1
Hazard <jejcneps@gmail.com>
2024-06-20 13:35:37 -0400

    removed tracked files that shouldn't be tracked

--- public/resources/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { test.core.init_BANG_(); } catch (e) { console.error("An error occurred when calling (test.core/init!)"); console.error(e); }
